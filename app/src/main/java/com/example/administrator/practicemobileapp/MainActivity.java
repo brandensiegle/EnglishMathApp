d299cc00ce5584d4ccc52540e1294edda06e02b2
Branden Siegle <brandensiegle@hotmail.com>
2017-06-04 14:36:25 +0900

    Solves static problem
    TODO: Randomize Problems
    TODO: Formatting
    TODO: Hints

--- app/src/main/java/com/example/administrator/practicemobileapp/MainActivity.java
+++ app/src/main/java/com/example/administrator/practicemobileapp/MainActivity.java
@@ -1,13 +1,17 @@
 package com.example.administrator.practicemobileapp;
 
+import android.content.Context;
+import android.content.DialogInterface;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
-import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
@@ -17,21 +21,92 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        toolbar.setTitle(R.string.title_bar);
+
         setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
+        Button showButton = (Button) findViewById(R.id.button);
+        showButton.setText("Solve Problem");
+        showButton.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+            public void onClick(View v) {
+                solveAProblem();
             }
         });
 
-        changeText cText = new changeText();
 
-        //Pass TextView to change and the text to change to
-        cText.changeTo((TextView) findViewById(R.id.textView2), "NewText");
+
+    }
+
+    public void returnToMainScreen(){
+        setContentView(R.layout.activity_main);
+        Button showButton = (Button) findViewById(R.id.button);
+        showButton.setText("Solve Problem");
+        showButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                solveAProblem();
+            }
+        });
+    }
+
+    public void solveAProblem(){
+        setContentView(R.layout.solve_problem);
+        Button returnButton = (Button) findViewById(R.id.returnButton);
+        returnButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                returnToMainScreen();
+            }
+        });
+
+        final String[] problem = MathProblemGenerator.getProblem();
+        TextView first = (TextView) findViewById(R.id.math_firstvalue);
+        TextView second = (TextView) findViewById(R.id.math_secondvalue);
+        TextView symbol = (TextView) findViewById(R.id.math_symbol);
+        TextView answer = (TextView) findViewById(R.id.math_answer);
+
+        first.setText(problem[0]);
+        second.setText(problem[1]);
+        symbol.setText(problem[2]);
+
+        FloatingActionButton answerCheckbutton = (FloatingActionButton) findViewById(R.id.checkAnswer);
+        answerCheckbutton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                EditText answerBox = (EditText) findViewById(R.id.math_answer);
+                if(answerBox.getText().toString().equalsIgnoreCase(problem[3])){
+                    showAlert("Correct!");
+                    solveAProblem();
+                } else {
+                    showAlert(answerBox.getText().toString().toLowerCase());
+                }
+            }
+        });
+
+    }
+
+    public void showAlert(String string){
+        AlertDialog.Builder diag = new AlertDialog.Builder(this);
+        diag.setTitle(R.string.popTitle);
+        if (string==null) {
+            diag.setMessage(R.string.popMessage);
+        } else {
+            diag.setMessage(string);
+        }
+        diag.setPositiveButton(R.string.popYes, new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+
+            }
+        });
+        diag.setNegativeButton(R.string.popNo, new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+
+            }
+        });
+        diag.show();
     }
 
     @Override
@@ -51,6 +126,8 @@ public class MainActivity extends AppCompatActivity {
         //noinspection SimplifiableIfStatement
         if (id == R.id.action_settings) {
             return true;
+        } else if (id == R.id.close){
+            System.exit(0);
         }
 
         return super.onOptionsItemSelected(item);
---
+++ app/src/main/java/com/example/administrator/practicemobileapp/MathProblemGenerator.java
@@ -0,0 +1,23 @@
+package com.example.administrator.practicemobileapp;
+
+/**
+ * Created by Administrator on 2017-06-04.
+ */
+
+public class MathProblemGenerator {
+
+    public MathProblemGenerator(){
+
+    }
+
+    public static String[] getProblem(){
+        String[] problem = new String[4];
+
+        problem[0] = "one";
+        problem[1] = "two";
+        problem[2] = "+";
+        problem[3] = "three";
+
+        return problem;
+    }
+}
